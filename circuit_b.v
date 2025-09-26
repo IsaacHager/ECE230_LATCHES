2b33087da5b3b4a2b9d9561da904411ecab27dd2
Isaac Hager <93684911+IsaacHager@users.noreply.github.com>
2025-09-26 10:00:42 -0600

    lab files

---
+++ circuit_b.v
@@ -0,0 +1,8 @@
+module circuit_b(
+    input A, B, C, D,// Declare inputs
+    output Y// Declare Y output
+);
+
+    assign Y = (B & ~D) | (~C & ~D) | (A & B);// Enter logic equation here
+
+endmodule
